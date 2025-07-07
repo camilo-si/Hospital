80f67475c8c2bc9651052f4eba31a7b265e03b83
camilo-si <ca.contrerash@duocuc.cl>
2025-07-07 17:46:25 -0400

    Delete Hospital directory

--- Hospital/src/main/java/com/example/ActHos/ActividadHospital_19_05/repository/EspecialidadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ActHos.ActividadHospital_19_05.repository;
-
-import com.example.ActHos.ActividadHospital_19_05.model.Especialidad;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EspecialidadRepository extends JpaRepository<Especialidad,Integer> {
-}
